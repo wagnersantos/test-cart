47e536aa8b320560515f243423a164899742b490
wagnersantos <wagtvc@gmail.com>
2020-04-20 08:05:22 -0300

    added colors

---
+++ src/core/assets/styles/colors.js
@@ -0,0 +1,10 @@
+export default {
+  green: 'rgb(60, 186, 146)',
+  background: 'rgb(241, 242, 246)',
+  black: 'rgb(46, 41, 38)',
+  red: 'rgb(223, 35, 30)',
+
+  white: 'rgb(255,255,255)',
+
+  transparent: 'transparent',
+};
