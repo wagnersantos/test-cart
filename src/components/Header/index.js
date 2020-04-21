4f88e7eccbf6915a66126fcfc2d0ef16860424b0
wagnersantos <wagtvc@gmail.com>
2020-04-21 11:34:26 -0300

    change height in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -37,6 +37,7 @@ const AppHeader = ({ title }) => {
     </StyledHeader>
   );
 };
+
 AppHeader.propTypes = {
   title: PropTypes.string.isRequired,
 };
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,10 +1,17 @@
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
 import { Header, Icon, Title } from 'native-base';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 import { colors } from 'core/assets/styles';
 
+const isIos = Platform.OS === 'ios';
+const headerHeight = isIos ? 54 : 30;
+
 export const StyledHeader = styled(Header)`
   background-color: ${colors.green};
+  height: ${headerHeight + getStatusBarHeight()}px;
+  padding-top: ${getStatusBarHeight() / 2}px;
 `;
 
 export const StyledIcon = styled(Icon)`
