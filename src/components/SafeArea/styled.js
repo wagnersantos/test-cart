b5892d99c1561241427507307aa49d83b1c17edc
wagnersantos <wagtvc@gmail.com>
2020-04-21 05:26:46 -0300

    change color in safearea

--- src/components/SafeArea/styled.js
+++ src/components/SafeArea/styled.js
@@ -3,6 +3,5 @@ import styled from 'styled-components/native';
 import { colors } from 'core/assets/styles';
 
 export const Container = styled.SafeAreaView`
-  flex: 1;
-  background-color: ${colors.background};
+  background-color: ${colors.green};
 `;
